fbe6144423817a4f46a2906b9b7cfe90c17a6e7e
Hassan Motiei <123471966+HassanMotiei@users.noreply.github.com>
2024-01-17 15:09:01 +0330

    add project to github

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default async function Home() {
+
+  return (
+    <main>
+      <h1>Home Page</h1>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,62 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1300px",
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+  plugins: [require("daisyui")],
+} satisfies Config;
